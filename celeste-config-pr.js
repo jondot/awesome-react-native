a0da2979e30333479b17f72079cce022051f9024
Ely Deckers <e.deckers@gmail.com>
2020-11-30 16:20:11 +0100

    Disable Celeste GitHub publication

--- celeste-config-pr.js
+++ celeste-config-pr.js
@@ -2,10 +2,6 @@ module.exports = {
     publishers: {
         file: true,
         github: {
-            auth: {
-                type: 'token',
-                token: process.env.CELESTE_GH
-            },
             context: {
                 ref: process.env.TRAVIS_PULL_REQUEST_SHA,
                 repo: 'awesome-react-native',
